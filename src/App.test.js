4e40f3e37311a63458cf4faf29539bc6865e65b0
alisabeata <alisa314159@gmail.com>
2019-02-10 21:34:46 +0200

    add App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  const wrapper = shallow(<App />);
+
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('Header length', () => {
+    expect(wrapper.find('Header').length).toEqual(1);
+  });
+
+  it('Balance length', () => {
+    expect(wrapper.find('Connect(Balance)').length).toEqual(1);
+  });
+
+  it('Exchange length', () => {
+    expect(wrapper.find('Exchange').length).toEqual(1);
+  });
+
+  it('ChartArea length', () => {
+    expect(wrapper.find('ChartArea').length).toEqual(1);
+  });
 });
